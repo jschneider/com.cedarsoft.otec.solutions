0cb993e8f65d34534a50663fca66e12c2a9a3ae7
Johannes Schneider <js@cedarsoft.com>
2015-06-25 22:55:01 +0200

    storing executed commands

--- src/main/java/com/cedarsoft/otec/live/Joystick.java
+++ src/main/java/com/cedarsoft/otec/live/Joystick.java
@@ -1,5 +1,8 @@
 package com.cedarsoft.otec.live;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents a joystick which has 4 buttons
  * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
@@ -9,6 +12,8 @@ public class Joystick {
   private final Command bCommand;
   private final Command xCommand;
   private final Command yCommand;
+
+  private final List<Command> executedCommands = new ArrayList<Command>();
 
   public Joystick( Command aCommand, Command bCommand, Command xCommand, Command yCommand ) {
     this.aCommand = aCommand;
@@ -20,20 +25,24 @@ public class Joystick {
   public void buttonAXPressed() {
     System.out.println( "Joystick.buttonAXPressed" );
     aCommand.execute();
+    executedCommands.add( aCommand );
   }
 
   public void buttonBXPressed() {
     System.out.println( "Joystick.buttonBXPressed" );
     bCommand.execute();
+    executedCommands.add( bCommand );
   }
 
   public void buttonXPressed() {
     System.out.println( "Joystick.buttonXPressed" );
     xCommand.execute();
+    executedCommands.add( xCommand );
   }
 
   public void buttonYXPressed() {
     System.out.println( "Joystick.buttonYXPressed" );
     yCommand.execute();
+    executedCommands.add( yCommand );
   }
 }
