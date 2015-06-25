ab87ac0b30c66321ea9a553fd5228831f58731b9
Johannes Schneider <js@cedarsoft.com>
2015-06-25 22:48:33 +0200

    added basic joystick class

---
+++ src/main/java/com/cedarsoft/otec/live/Joystick.java
@@ -0,0 +1,23 @@
+package com.cedarsoft.otec.live;
+
+/**
+ * Represents a joystick which has 4 buttons
+ * @author Johannes Schneider (<a href="mailto:js@cedarsoft.com">js@cedarsoft.com</a>)
+ */
+public class Joystick {
+  public void buttonAXPressed() {
+    System.out.println( "Joystick.buttonAXPressed" );
+  }
+
+  public void buttonBXPressed() {
+    System.out.println( "Joystick.buttonBXPressed" );
+  }
+
+  public void buttonXPressed() {
+    System.out.println( "Joystick.buttonXPressed" );
+  }
+
+  public void buttonYXPressed() {
+    System.out.println( "Joystick.buttonYXPressed" );
+  }
+}
